f20f62b7b2a06fd2c613391a72816e8767187eec
Solijon Sharipov <40540002+Solijons@users.noreply.github.com>
2019-09-02 12:27:09 -1000

    Multidimensional example in Java

---
+++ multiDemencinal.java
@@ -0,0 +1,15 @@
+ public static void  main(String[] args)  {
+        int a[][] = new int[2][3];
+        a[0][0] = 2;
+        a[0][1] = 3;
+        a[0][2] = 4;
+        a[1][0] = 5;
+        a[1][1] = 4;
+        a[1][2] = 3;
+
+        for(int i =0; i < 2; i ++){
+            for(int j = 0; j < 3; j++) {
+                System.out.println(a[i][j]);
+            }
+        }
+}

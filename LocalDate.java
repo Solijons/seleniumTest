0c3b8d2f5ba7c7b2513c2aead67810d37ddfdbbc
Solijon Sharipov <40540002+Solijons@users.noreply.github.com>
2019-11-25 10:55:21 -1000

    Converting String to Localdate
    Alternative way of converting input "date" from attribute of WebElement's value to Date in Java.

--- LocalDate.java
+++ LocalDate.java
@@ -3,3 +3,13 @@
     dateInput.sendKeys(DateTimeFormatter.ofPattern("MM/dd/yyy").format(localDate));
 
     String value = driver.findElement(By.id("date")).getAttribute("value");
+
+    // Perquisites 
+    protected String getAttrValue(final By locator) {
+        String value = driver.findElement(locator).getAttribute("value");
+        return value;
+    }
+
+    // Alternative way
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    LocalDate Date = LocalDate.parse(getAttrValue(By.id("date-picker-dialog")), dtf);
